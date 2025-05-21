578becd6ea3fc79938413419fd05ab42eae3a0de
nicholas-vasconcelos <nicholasbvasconcelos@gmail.com>
2025-05-21 10:38:14 -0300

    Delete temp directory
    agora skydrop

--- temp/myapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Usuario, Restaurante, ItemMenu, Drone, Pedido
-
-admin.site.register(Usuario)
-admin.site.register(Restaurante)
-admin.site.register(ItemMenu)
-admin.site.register(Drone)
-admin.site.register(Pedido)
